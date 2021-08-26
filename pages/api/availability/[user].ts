mychiefs58js/cal.com

commit 6c9b193c67ecb8cdc51071ee4082bfc1d64bf456

Temporarily disable checking video busy times

pages/api/availability/[user].ts
@@ -31,12 +31,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     req.query.dateTo,
     selectedCalendars
   );
-  const videoBusyTimes = await getBusyVideoTimes(
-    currentUser.credentials,
-    req.query.dateFrom,
-    req.query.dateTo
-  );
-  calendarBusyTimes.push(...videoBusyTimes);
+  // const videoBusyTimes = await getBusyVideoTimes(
+  //   currentUser.credentials,
+  //   req.query.dateFrom,
+  //   req.query.dateTo
+  // );
+  // calendarBusyTimes.push(...videoBusyTimes);
 
   const bufferedBusyTimes = calendarBusyTimes.map((a) => ({
     start: dayjs(a.start).subtract(currentUser.bufferTime, "minute").toString(),

commit e11dd0c7d095171f7a6a4de8dee3fc017e4a33aa

fix linting issue introduced in 6c9b193 (#529)

pages/api/availability/[user].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@lib/prisma";
 import { getBusyCalendarTimes } from "@lib/calendarClient";
-import { getBusyVideoTimes } from "@lib/videoClient";
+// import { getBusyVideoTimes } from "@lib/videoClient";
 import dayjs from "dayjs";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {